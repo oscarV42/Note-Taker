0e7e9d0a329f710a7660e929a642723034254822
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-02 01:01:21 -0400

    implementing middleware for parsing

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const fs = require('fs');
 const util = require('util');
 
 const app = express();
+
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
