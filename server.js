be3081f6b2620c320b7ce3741d3ec3d9b9c23e3f
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-02 01:07:31 -0400

    Getting route for landing page

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//GET route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 //Get route for /notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
