47b901a72b539d0427a70fe8e6eba8e12e58bfa6
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-02 23:25:21 -0400

    creating function to send data to the JSON file

--- server.js
+++ server.js
@@ -24,3 +24,15 @@ app.get('/notes', (req, res) => {
 
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const WriteToFile = (destination, content) => {
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
+        err ? console.log(err) : console.info(`\nData written to ${destination}`)
+    );
+}
