3e3cb2e60a041f63a43283469601c01d868ba0a0
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-13 00:18:22 -0400

    GET route that returns any specific note according to relativeid

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,24 @@ const { readFromFile, readAndAppend, readAndDelete } =  require('../helpers/fsUt
 const  uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
+app.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET route that returns any specific id
+app.get('/:id', (req, res) => {
+    // Coerce the specific search term to lowercase
+    const requestedId = req.params.id;
+    const dataHelper = readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    // Iterate through the terms name to check if it matches `req.params.term`
+    for (let i = 0; i < dataHelper.length; i++) {
+      if (requestedId === dataHelper[i].id) {
+        return res.json(dataHelper[i]);
+      }else{
+        // Return a message if the term doesn't exist in our DB
+        return res.json('No match found');
+      }
+    }
+  
 });
