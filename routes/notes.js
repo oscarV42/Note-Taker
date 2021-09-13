51fc228bb28e519d1cea03ad716baa4ac670df6c
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-13 00:15:02 -0400

    creating function to GET route for retrieving all notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,9 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, readAndDelete } =  require('../helpers/fsUtils');
 const  uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
